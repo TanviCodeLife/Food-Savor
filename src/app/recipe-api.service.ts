9b088dd081afc7ca7b9422a984130badf6e98455
Brooke Perry, Paige Williams and Tanvi Garg <brooke+paige+tanvi@>
2019-02-04 15:15:27 -0800

    add diet as parameter to getByIngredients()

--- src/app/recipe-api.service.ts
+++ src/app/recipe-api.service.ts
@@ -8,8 +8,11 @@ export class RecipeApiService {
 
   constructor(private http: Http) { }
 
-  getByIngredients(ingredient: string, health: string ){
+  getByIngredients(ingredient: string, health: string, diet: string ){
     return this.http.get(`https://api.edamam.com/search?q=${ingredient}&app_id=${recipeApiAppId}&app_key=${recipeApiKey}&to=20&health=${health}`);
   }
+
+  if (diet == null) then
+
 
 }
