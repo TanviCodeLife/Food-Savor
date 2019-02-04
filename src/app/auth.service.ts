f6dfe9fbd2d459b180bae8811126460c05283e10
Kristin Brewer-Lowe and Sheila Stephen <kristin@>
2019-02-04 14:35:18 -0800

    Add update preference method to auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
@@ -6,7 +7,7 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class AuthService {
   user: Observable<firebase.User>;
-  constructor( public afAuth: AngularFireAuth ) {
+  constructor( private database: AngularFireDatabase, public afAuth: AngularFireAuth ) {
     this.user = afAuth.authState;
    }
 
@@ -18,6 +19,14 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  updatePreferences(preferences){
+    this.user.subscribe(user => {
+      let userPreference = this.database.object(`preferences/${user.uid}`)
+      userPreference.set(preferences);
+      console.log(user.uid);
+    })
   }
 
 
--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-preferences',
   templateUrl: './preferences.component.html',
-  styleUrls: ['./preferences.component.css']
+  styleUrls: ['./preferences.component.css'],
+  providers: [ AuthService ]
 })
 export class PreferencesComponent implements OnInit {
-  allergies: object[] = [{"apiCode": "peanut-free", "name": "Peanuts"},
-    {"code": "peanut-free", "name": "Peanuts"},
-    {"code": "tree-nut-free", "name": "Tree Nuts"},
-    {"code": "soy-free", "name": "Soy"},
-    {"code": "fish-free", "name": "Fish"},
-    {"code": "shellfish-free", "name": "Shelfish"},
-    {"code": "dairy-free", "name": "Dairy"},
-    {"code": "egg-free", "name": "Eggs"},
-    {"code": "gluten-free", "name": "Gluten"},
-    {"code": "wheat-free", "name": "Wheat"},
+  preferences: object[] = [
+    {"code": "high-protein", "name": "High protein", "checked": false},
+    {"code": "low-fat", "name": "Low fat", "checked": false},
+    {"code": "low-carb", "name": "Low carb", "checked": false},
+    {"code": "vegan", "name": "Vegan", "checked": false},
+    {"code": "vegetarian", "name": "Vegetarian", "checked": false},
+    {"code": "peanut-free", "name": "Peanut free", "checked": false},
+    {"code": "tree-nut-free", "name": "Tree nut free", "checked": false},
+    {"code": "sugar-conscious", "name": "Sugar conscious", "checked": false},
+    {"code": "alcohol-free", "name": "Alcohol free", "checked": false}
   ];
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  updatePreference(value){
+    for (let i = 0; i < this.preferences.length; i++) {
+      if (this.preferences[i].code === value) {
+        this.preferences[i].checked = !this.preferences[i].checked;
+      }
+    }
+    console.log(this.preferences);
+    this.authService.updatePreferences(this.preferences);
   }
 
 }
