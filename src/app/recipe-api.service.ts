f0fcb7eb6ed2fe59f377fd39730328a80f6b7e76
Brooke Perry, Paige Williams and Tanvi Garg <paige@>
2019-02-04 14:47:36 -0800

    make recipe api call and get response using recipe service

--- src/app/recipe-api.service.ts
+++ src/app/recipe-api.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { recipeApiAppId, recipeApiKey } from './api-keys';
 
 @Injectable()
 export class RecipeApiService {
 
   constructor(private http: Http) { }
+
+  getByIngredients(ingredient: string, health: string ){
+    return this.http.get(`https://api.edamam.com/search?q=${ingredient}&app_id=${recipeApiAppId}&app_key=${recipeApiKey}&to=20&health=${health}`);
+  }
 
 }
