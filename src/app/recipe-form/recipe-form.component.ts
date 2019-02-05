d17d442308b0d9c60d62486ccb4e80cef3a6d9fe
Brooke Perry, Kristin Brewer-Lowe and Paige Williams <kristin@>
2019-02-05 10:08:49 -0800

    Separate preferences into health and diet.

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -8,10 +8,7 @@ import { RecipeApiService } from '../recipe-api.service';
   providers: [RecipeApiService]
 })
 export class RecipeFormComponent implements OnInit {
-  preferences: Preference = [
-    {"code": "high-protein", "name": "High protein", "checked": false},
-    {"code": "low-fat", "name": "Low fat", "checked": false},
-    {"code": "low-carb", "name": "Low carb", "checked": false},
+  healths: Preference = [
     {"code": "vegan", "name": "Vegan", "checked": false},
     {"code": "vegetarian", "name": "Vegetarian", "checked": false},
     {"code": "peanut-free", "name": "Peanut free", "checked": false},
@@ -19,30 +16,62 @@ export class RecipeFormComponent implements OnInit {
     {"code": "sugar-conscious", "name": "Sugar conscious", "checked": false},
     {"code": "alcohol-free", "name": "Alcohol free", "checked": false}
   ];
-  apiCodes: string[] = [];
 
+  diets: Preference = [
+    {"code": "high-protein", "name": "High protein", "checked": false},
+    {"code": "low-fat", "name": "Low fat", "checked": false},
+    {"code": "low-carb", "name": "Low carb", "checked": false},
+  ]
+  apiDiet: string[] = [];
+  apiHealth: string[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
-  updatePreference(value){
-    for (let i = 0; i < this.preferences.length; i++) {
-      if (this.preferences[i].code === value) {
-        this.preferences[i].checked = !this.preferences[i].checked;
+  updateDiet(value) {
+    for (let i = 0; i < this.diets.length; i++) {
+      if (this.diets[i].code === value) {
+        this.diets[i].checked = !this.diets[i].checked;
       }
     }
-    console.log(this.preferences);
+    console.log("diets" + this.diets);
   }
 
-  createApiCode() {
-    this.apiCodes = [];
-    for (let i = 0; i < this.preferences.length; i++) {
-      if (this.preferences[i].checked === true) {
-        this.apiCodes.push(this.preferences[i].code)
+  updateHealth(value) {
+    for (let i = 0; i < this.healths.length; i++) {
+      if (this.healths[i].code === value) {
+        this.healths[i].checked = !this.healths[i].checked;
       }
     }
-    console.log(this.apiCodes);
+    console.log("healths" + this.healths);
+  }
+
+  // updatePreference(value){
+  //   for (let i = 0; i < this.preferences.length; i++) {
+  //     if (this.preferences[i].code === value) {
+  //       this.preferences[i].checked = !this.preferences[i].checked;
+  //     }
+  //   }
+  //   console.log("preferences" + this.preferences);
+  // }
+
+  createApiCode() {
+    this.apiDiet = [];
+    this.apiHealth = [];
+    for (let i = 0; i < this.diets.length; i++) {
+      if (this.diets[i].checked === true) {
+        this.apiDiet.push(this.diets[i].code)
+      }
+    }
+    console.log("Diet: " + this.apiDiet);
+
+    for (let i = 0; i < this.healths.length; i++) {
+      if (this.healths[i].checked === true) {
+        this.apiHealth.push(this.healths[i].code)
+      }
+    }
+    console.log("Health: " + this.apiHealth);
   }
 
 }
