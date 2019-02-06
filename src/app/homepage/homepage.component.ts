4a7a3edb80c6cb5bf9f40dec03783a1bc99700f9
brooke perry and Kristin Brewer-Lowe <brooke@>
2019-02-06 10:03:59 -0800

    style login/logout

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { RecipeApiService } from '../recipe-api.service';
 
 @Component({
   selector: 'homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
-  providers: [ AuthService ]
+  providers: [ AuthService, RecipeApiService]
 })
 export class HomepageComponent implements OnInit {
-
-  constructor(private authService: AuthService) { }
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
+  constructor(private authService: AuthService, private recipeApiService: RecipeApiService) {
+    this.authService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+      }
+    });
+  }
 
   getFavorites(){
     this.authService.getFavorites();
   }
 
+
   ngOnInit() {
+
   }
 
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
